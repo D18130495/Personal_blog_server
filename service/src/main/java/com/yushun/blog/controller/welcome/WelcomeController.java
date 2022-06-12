545f962ab12078d0c39a34a463f4ad293ef03e7e
D18130495 <D18130495@mytudublin.ie>
2022-06-12 23:58:53 +0100

    Created welcome page

---
+++ service/src/main/java/com/yushun/blog/controller/welcome/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.yushun.blog.controller.welcome;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * welcome controller
+ * </p>
+ *
+ * @author yushun zeng
+ * @since 2022-6-12
+ */
+
+@CrossOrigin
+@RestController
+@RequestMapping("/")
+public class WelcomeController {
+    @GetMapping("")
+    public String welcome() {
+        String welcome = "Welcome to yushun's personal blog back-end.";
+        return welcome;
+    }
+}
